3e73b3939f182acf21c567948597953f7b8a63b8
chronossc <philipe.rp@gmail.com>
2010-08-05 12:26:29 +0000

    Adding setup.py
    git-svn-id: https://django-breadcrumbs.googlecode.com/svn/trunk@13 e9f8713c-886b-11de-b5b6-bfb8bb3c0290

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name="django-breadcrumbs",
+    version="1.1.0",
+    description="Easy to use generic breadcrumbs system for Django framework.",
+    author="Felipe 'chronos' Prenholato",
+    author_email="philipe.rp@gmail.com",
+    url="http://github.com/chronossc/django-breadcrumbs",
+    packages=['breadcrumbs'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ]
+)
