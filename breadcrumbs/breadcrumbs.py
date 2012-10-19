f71c0dd9e11a0759338f0bd0f71673b6c660e1b8
Felipe Prenholato <philipe.rp@gmail.com>
2012-10-19 09:47:40 -0300

    Remove old code.

--- breadcrumbs/breadcrumbs.py
+++ breadcrumbs/breadcrumbs.py
@@ -8,23 +8,6 @@ from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
 
 
-# class Singleton(object):
-#     """
-#     We use a simple singleton pattern in Breadcrumbs.
-#     Example from http://svn.ademar.org/code/trunk/junk-code/singleton_vs_borg.py
-#     """
-#     def __new__(cls, *args, **kwds):
-#         it = cls.__dict__.get("__it__")
-#         if it is not None:
-#             return it
-#         cls.__it__ = it = object.__new__(cls)
-#         it._1st_init(*args, **kwds)
-#         return it
-
-#     def _1st_init(self, *args, **kwds):
-#         pass
-
-
 class BreadcrumbsInvalidFormat(Exception):
     """
     Simple exception that can be extended
