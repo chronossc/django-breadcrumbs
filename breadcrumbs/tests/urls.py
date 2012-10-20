5f38229657556740ede4d35f64f62672025005fb
Felipe Prenholato <philipe.rp@gmail.com>
2012-10-20 00:47:50 -0300

    fix pattern import that changed from 1.3.x to 1.4.x

--- breadcrumbs/tests/urls.py
+++ breadcrumbs/tests/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls import patterns, include
+import django
+if django.get_version().startswith('1.4'):
+    from django.conf.urls import patterns
+else:
+    from django.conf.urls.defaults import patterns
 from .views import page1
 # special urls for flatpage test cases
 urlpatterns = patterns('',
