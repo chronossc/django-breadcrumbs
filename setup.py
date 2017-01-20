5b08d87894f685e389c4883b496c743aafa14531
Erik Telepovský <erik.telepovsky@gmail.com>
2017-01-20 09:56:24 +0100

    Version bump to 1.1.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="django-breadcrumbs",
-    version="1.1.3-p1",
+    version="1.1.4",
     packages=find_packages(exclude=('breadcrumbs_sample*', 'sample_d14*')),
     author="Felipe 'chronos' Prenholato",
     author_email="philipe.rp@gmail.com",
